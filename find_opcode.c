0da1ed216167e9b938939d50bb30852eac3f38db
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-12-11 23:09:52 +0000

    make opcode file

--- find_opcode.c
+++ find_opcode.c
@@ -1,17 +1,29 @@
 #include "monty.h"
 
+instruction_t instruction[] =
+    {
+        {"push", push},
+        {"pall", pall},
+        {"pint", pint},
+        {"pop", pop},
+        {"swap", swap},
+        {"add", add},
+        {"nop", nop},
+};
+
 /***/
 
-void find_opcode(void)
+void find_opcode(char *opcode, stack_t **stack, unsigned int line_number)
 {
-    instruction_t instruction[] =
+    int i;
+
+    for (i = 0; i < sizeof(instruction) / sizeof(instruction[0]); i++)
+    {
+        if (strcmp(opcode, instruction[i].opcode) == 0)
         {
-            {"push", push},
-            {"pall", pall},
-            {"pint", pint},
-            {"pop", pop},
-            {"swap", swap},
-            {"add", add},
-            {"nop", nop},
-        };
+            instruction[i].f(stack, line_number);
+            return;
+        }
+    }
+    printf("Unknown opcode: %s\n", opcode);
 }
