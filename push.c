a5edc5f94e0ce6407462715acbf9062fa4a60d8b
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-12-11 06:39:18 +0000

    define push

--- push.c
+++ push.c
@@ -4,5 +4,15 @@
 
 void push(stack_t **stack, unsigned int line_number)
 {
-    ...
+	stack_t *new_node;
+	new_node = malloc(sizeof(stack_t));
+
+	if (new_node == NULL)
+	{
+		fprintf("Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+	new_node->line_number = line_number;
+	new_node->next = *stack;
+	*stack - new_node;
 }
