4c87c1a0372c290461ee7168d90ac22e236abc7e
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-12-11 22:39:44 +0000

    write pall code

--- pall.c
+++ pall.c
@@ -4,5 +4,13 @@
 
 void pall(stack_t **stack, unsigned int line_number)
 {
-    ...
+    stack_t *current;
+
+    current = *stack;
+
+    while (current != NULL)
+    {
+        printf("%d\n", current->n);
+        current = current->next;
+    }
 }
