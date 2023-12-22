01233a2c33c8f0cac843928562577436b946a1c8
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-12-22 07:42:12 +0000

    edits to findopcode

--- find_opcode.c
+++ find_opcode.c
@@ -1,29 +1,34 @@
 #include "monty.h"
 
 instruction_t instruction[] =
-    {
-        {"push", push},
-        {"pall", pall},
-        {"pint", pint},
-        {"pop", pop},
-        {"swap", swap},
-        {"add", add},
-        {"nop", nop},
+	{
+		{"push", (void (*)(stack_t **, unsigned int))push},
+		{"pall", (void (*)(stack_t **, unsigned int))pall},
+		{"pint", (void (*)(stack_t **, unsigned int))pint},
+		{"pop", (void (*)(stack_t **, unsigned int))pop},
+		{"swap", (void (*)(stack_t **, unsigned int))swap},
+		{"add", (void (*)(stack_t **, unsigned int))add},
+		{"nop", (void (*)(stack_t **, unsigned int))nop},
 };
 
-/***/
+/**
+ * find_opcode - finds the opcode to use
+ * @opcode: opcode to search for
+ * @stack: stack to search in
+ * @line_number: line number
+ */
 
 void find_opcode(char *opcode, stack_t **stack, unsigned int line_number)
 {
-    size_t i;
+	size_t i;
 
-    for (i = 0; i < sizeof(instruction) / sizeof(instruction[0]); i++)
-    {
-        if (strcmp(opcode, instruction[i].opcode) == 0)
-        {
-            instruction[i].f(stack, line_number);
-            return;
-        }
-    }
-    printf("Unknown opcode: %s\n", opcode);
+	for (i = 0; i < sizeof(instruction) / sizeof(instruction[0]); i++)
+	{
+		if (strcmp(opcode, instruction[i].opcode) == 0)
+		{
+			instruction[i].f(stack, line_number);
+			return;
+		}
+	}
+	printf("Unknown opcode: %s\n", opcode);
 }
