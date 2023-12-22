c9f26c34b648adc9910f6fb61945f962e92ec746
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-12-22 08:42:00 +0000

    make add and swap

--- add.c
+++ add.c
@@ -1,9 +1,28 @@
 #include "monty.h"
 
 /**
+ * add - adds the top two elements of the stack
+ * @stack: pointer to beginning of stack
+ * @line_number: current line number
+ */
 
 void add(stack_t **stack, unsigned int line_number)
 {
-    ...
+	int result;
+	stack_t *temp;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	result = (*stack)->n + (*stack)->next->n;
+
+	(*stack)->next->n = result;
+
+	temp = *stack;
+	*stack = (*stack)->next;
+	(*stack)->prev = NULL;
+	free(temp);
 }
-*/
--- swap.c
+++ swap.c
@@ -1,9 +1,37 @@
 #include "monty.h"
 
 /**
+ * swap - swaps top two elements of a stack
+ * @stack: pointer to beginning of stack
+ * @line_number: current line number
+ */
 
 void swap(stack_t **stack, unsigned int line_number)
 {
-    ...
+	stack_t *top, *second_top;
+
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	top = *stack;
+	second_top = (*stack)->next;
+
+	top->next = second_top->next;
+	if (top->next != NULL)
+	{
+		top->next->prev = top;
+	}
+
+	second_top->next = top;
+	second_top->prev = top->prev;
+	if (second_top->prev != NULL)
+	{
+		second_top->prev->next = second_top;
+	}
+
+	top->prev = second_top;
+	*stack = second_top;
 }
-    */
