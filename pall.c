e7ef758194a94ab27e40b337c68c518f99477826
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-12-22 08:20:10 +0000

    update pall proto and header

--- pall.c
+++ pall.c
@@ -6,11 +6,9 @@
  * @line_number: line number being executed
  */
 
-void pall(stack_t **stack, unsigned int line_number)
+void pall(stack_t **stack)
 {
 	stack_t *current = *stack;
-
-	UNUSED(line_number);
 
 	while (current)
 	{
