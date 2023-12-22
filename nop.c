c28e92a50406664834aa0916b761e01615aeb630
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-12-22 08:43:14 +0000

    make nop

--- nop.c
+++ nop.c
@@ -1,9 +1,11 @@
 #include "monty.h"
 
 /**
+ * nop - does nothing
+ * @stack: pointer to beginning of stack
+ * @line_number: current line number
+ */
 
 void nop(stack_t **stack, unsigned int line_number)
 {
-    ...
 }
-    */
