fb43d6653c60b0c3e4902791d656e6b79a833d7b
clay-creates <122416165+clay-creates@users.noreply.github.com>
2023-12-22 07:44:11 +0000

    revert opcode

--- find_opcode.c
+++ find_opcode.c
@@ -2,13 +2,13 @@
 
 instruction_t instruction[] =
 	{
-		{"push", (void (*)(stack_t **, unsigned int))push},
-		{"pall", (void (*)(stack_t **, unsigned int))pall},
-		{"pint", (void (*)(stack_t **, unsigned int))pint},
-		{"pop", (void (*)(stack_t **, unsigned int))pop},
-		{"swap", (void (*)(stack_t **, unsigned int))swap},
-		{"add", (void (*)(stack_t **, unsigned int))add},
-		{"nop", (void (*)(stack_t **, unsigned int))nop},
+		{"push", push},
+		{"pall", pall},
+		{"pint", pint},
+		{"pop", pop},
+		{"swap", swap},
+		{"add", add},
+		{"nop", nop},
 };
 
 /**
